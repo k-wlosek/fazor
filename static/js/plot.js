fd9b6284e52e3a25ba0a7f2f2ca485c436327c5d
kw <115936263+k-wlosek@users.noreply.github.com>
2023-12-22 16:13:24 +0100

    feat(website): :lipstick: Add input fields for new diagram drawing library
    Modifies index.html with neccessary fields to be used in phasors library. Adds code to plot.js to allow for multiple input fields, since library supports it.

--- static/js/plot.js
+++ static/js/plot.js
@@ -99,6 +99,85 @@ async function drawTrojkat() {
     fig_element.alt = document.getElementById("tr-title").value;
 }
 
+function addDatapoint(event) {
+    const button = event.target;
+    const datapoint = button.parentElement.parentElement;
+    let id = datapoint.id;
+    idList = id.split('-');
+    idList[2] = parseInt(idList[2]) + 1;
+    id = idList.join('-');
+    console.log(id);
+    const newDatapoint = datapoint.cloneNode(true);
+    newDatapoint.id = id;
+    datapoint.parentNode.insertBefore(newDatapoint, datapoint.nextSibling);
+}
+
+function addSymbol(event) {
+    const button = event.target;
+    const symbol = button.parentElement.parentElement;
+
+    // Get id by incrementing the last input id
+    inputElement = symbol.querySelector('input');
+    let id = inputElement.id;
+    idList = id.split('-');
+    idList[1] = parseInt(idList[1]) + 1;
+    id = idList.join('-');
+
+    const newSymbol = symbol.cloneNode(true);
+    newInputElement = newSymbol.querySelector('input');
+    newInputElement.id = id;
+
+    newDatapointElement = newSymbol.querySelector('div');
+    idList = id.split('-');
+    idList.splice(2, 0, "0");
+    idList[3] = "datapoint";
+    id = idList.join('-');
+    newDatapointElement.id = id;
+    // newSymbol.id = id;
+    symbol.parentNode.insertBefore(newSymbol, symbol.nextSibling);
+}
+
+function removeNode(event) {
+    const button = event.target;
+    const node = button.parentElement.parentElement;
+    node.remove();
+}
+
+function findOptions(el) {
+    const options = el.querySelectorAll('option');
+    const parentParent = el.parentElement.parentElement;
+
+    let divs = parentParent.querySelectorAll(':scope > div');
+    divs = Array.from(divs);
+    divs = divs.splice(1, divs.length - 2);
+    let units = [];
+    let j = 1;
+    divs.forEach((element, index) => {
+        units.push(element.querySelector('input').value + " (" + j + ".)");
+        j++;
+    });
+    // Update options
+    units.forEach(function(text, index) {
+        if (options[index]) {
+          // If the option exists, update its text
+          options[index].textContent = text;
+        } else {
+          // If the option doesn't exist, create a new option and append it to the select element
+          var newOption = document.createElement('option');
+          newOption.textContent = text;
+          el.appendChild(newOption);
+        }
+      });
+      
+      // Remove extra options if arr2 is shorter than the original options
+      if (options.length > units.length) {
+        for (var i = units.length; i < options.length; i++) {
+          options[i].remove();
+        }
+      }
+
+}
+
 async function drawFazory() {
     const pyodide = await loadPyodide({
         packages: ['matplotlib', 'numpy']
